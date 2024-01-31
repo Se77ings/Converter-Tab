99732d62cf11bc713af81c837e915b91afab9352
Se77ings <se77ings@gmail.com>
2024-01-31 12:39:23 -0300

    spaces fix

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 notas = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E','F', 'F#', 'G', 'G#']
 testes = 1
-# bar = input("digite a corda: ")
 bar = 'E'
 if(testes != 1):
     casa = int(input("digite a nota: "))
-# vamos começar pensando na corda mais grossa E
-#provavelmente isso faz mais sentido ser uma função, e o 'E' vai ser passado como um parametro
 def foo(casa, bar):
     for i in notas:
         if(i == bar):
@@ -22,7 +19,6 @@ def foo(casa, bar):
         notaDigitada = notas[nota_casa]
         contadora += 1
 
-    # print(notaDigitada)
     return notaDigitada
 
 def tecnicas(tec):
@@ -44,23 +40,19 @@ def trataString(str):
     novosTracos = 0
     removeTracos = 0
     if(str == 'breakRow'):
-        # print('\n')
         novaString = '\n'
         StringFinal += novaString
         return
     novaString = str[0:2]
-    i=2
+    i=1
     while True:
-        # print(str[i])
         i+=1
-        #identifica inteiros
         if(str[i]):
             if(str[i].isnumeric()):
                 if(str[i+1].isnumeric()):
                     numero = str[i] + str[i+1]
                     i +=1
                     novosTracos += 1
-                    #novaString += '-' #isso aqui que está adicionando um tracinho no lugar errado, fazer isso apenas quando o proximo caracter for == '|'
                 else:
                     numero = str[i]
                 retorno = foo(int(numero), str[0])
@@ -70,14 +62,8 @@ def trataString(str):
             #identifica as tecnicas
             elif (str[i].isalpha()):
                 novaString += tecnicas(str[i])
-            #retorna os espaços
-            # elif(str[i+1] == '|'):
-            #     while(novosTracos > 0):
-            #         novaString += '-'
-            #         novosTracos -= 1
-            #         break
             else:
-                if(str[i+1] == '|'):
+                if(str[i+1] == '-'):
                     while(novosTracos > 0):
                         novaString += '-'
                         novosTracos -= 1
@@ -88,7 +74,6 @@ def trataString(str):
 
             
             if(str[i] == '|'):
-                # print(novaString)
                 novaString += '\n'
                 StringFinal += novaString
                 break
@@ -108,3 +93,6 @@ with open('file.txt', 'r') as tab:
 
 
 print(StringFinal)
+
+with open('output.txt', 'w', encoding='utf-8') as tab:
+    tab.write(StringFinal)
